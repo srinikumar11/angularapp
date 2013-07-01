03af2cf9d56fcb59a5400f0fa42345296b858b04
Srinivasan Kumar <srini@comogroupap.com>
2013-07-01 11:56:47 +0530

    First commit

---
+++ js/main/app.js
@@ -0,0 +1,13 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: srini
+ * Date: 01/07/13
+ * Time: 9:07 AM
+ * To change this template use File | Settings | File Templates.
+ */
+var app = angular.module('MyTutorialApp',[]);
+
+app.config(function($interpolateProvider){
+        //$interpolateProvider.startSymbol('{[{').endSymbol('}]}');
+    }
+);
